d38974ec9790193659aff82946ed54d9499a8cc8
Karen Barbé <info@karenbarbe.com>
2025-09-16 17:46:50 -0500

    generate Delivery resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_16_224534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deliveries", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "description"
+    t.string "details"
+    t.date "supposed_to_arrive_on"
+    t.boolean "arrived"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
